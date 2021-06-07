f61d330219a4330b0a8cde3eb8316f1847ec7bca
Ramil <ramil.minyukov@yandex.ru>
2021-06-08 00:25:55 +0300

    Add QuestionType and Question models

--- backend/core/models.py
+++ backend/core/models.py
@@ -13,3 +13,28 @@ class Test(models.Model):
     random_order = models.BooleanField()
     author = models.ForeignKey(User, on_delete=SET_NULL, null=True)
 
+
+SINGLE, MULTIPLE, TEXT, MATCHING, SEQUENCE = range(5)
+TYPES = (
+    (SINGLE, "Single"),
+    (MULTIPLE, "Multiple"),
+    (TEXT, "Text"),
+    (MATCHING, "Matching"),
+    (SEQUENCE, "Sequence"),
+)
+
+
+class QuestionType(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.IntegerField(choices=TYPES, default=SINGLE)
+
+
+class Question(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500, null=True)
+    image = models.ImageField()
+    explanation = models.CharField(max_length=500, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    test = models.ForeignKey(Test, on_delete=SET_NULL, null=True)
+    type = models.ForeignKey(QuestionType, on_delete=SET_NULL, null=True)

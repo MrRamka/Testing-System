9658e3f9216e6f8cbb5cf0c892203c8d81635ff2
Ramil <ramil.minyukov@yandex.ru>
2021-06-08 00:52:06 +0300

    Add QuestionType and Question admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Test, QuestionType, Question
+
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'created_at', 'updated_at', 'random_order', 'author']
+    list_display_links = ['name']
+    readonly_fields = ['slug']
+
+
+@admin.register(QuestionType)
+class QuestionTypeAdmin(admin.ModelAdmin):
+    list_display = ['type']
+    list_display_links = ['type']
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at', 'updated_at', 'test', 'type']
+    list_display_links = ['name']
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,17 +1,30 @@
 from django.db import models
 from django.db.models import SET_NULL
-
+from django.utils.text import slugify
 from user.models import User
 
 
 class Test(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=500, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     slug = models.SlugField()
     random_order = models.BooleanField()
     author = models.ForeignKey(User, on_delete=SET_NULL, null=True)
+
+    def _generate_unique_slug(self):
+        unique_slug = slugify(self.name)
+        num = 1
+        while Test.objects.filter(slug=unique_slug).exists():
+            unique_slug = '{}-{}'.format(unique_slug, num)
+            num += 1
+        return unique_slug
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self._generate_unique_slug()
+        super().save(*args, **kwargs)
 
 
 SINGLE, MULTIPLE, TEXT, MATCHING, SEQUENCE = range(5)
@@ -25,16 +38,16 @@ TYPES = (
 
 
 class QuestionType(models.Model):
-    name = models.CharField(max_length=100)
     type = models.IntegerField(choices=TYPES, default=SINGLE)
 
 
 class Question(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=500, null=True)
-    image = models.ImageField()
-    explanation = models.CharField(max_length=500, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    image = models.ImageField(null=True, blank=True)
+    explanation = models.CharField(max_length=500, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     test = models.ForeignKey(Test, on_delete=SET_NULL, null=True)
-    type = models.ForeignKey(QuestionType, on_delete=SET_NULL, null=True)
+    type = models.IntegerField(choices=TYPES, default=SINGLE)
+

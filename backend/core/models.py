cb9cb3520c8078bb3213a4b975059887ab7992b0
Ramil <ramil.minyukov@yandex.ru>
2021-06-08 00:03:58 +0300

    Add Test model

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,2 +1,15 @@
 from django.db import models
+from django.db.models import SET_NULL
+
+from user.models import User
+
+
+class Test(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField()
+    random_order = models.BooleanField()
+    author = models.ForeignKey(User, on_delete=SET_NULL, null=True)
 
